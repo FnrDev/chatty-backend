0f9fbc104e6bfdce74b3da9901a5eca58da6b3be
Ahmed <fnr5541@gmail.com>
2026-08-07 19:17:50 +0300

    setup websocket

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express() // creates a express application
 const dotenv = require("dotenv").config() //this allows me to use my .env values in this file
 const morgan = require('morgan')
 const cors = require('cors')
+const { Server } = require('socket.io')
+const { createServer } = require('http')
 
 // Routes Import
 const authRoutes = require('./routes/auth.routes')
@@ -19,12 +21,31 @@ app.use(
 app.use(express.json())
 app.use(morgan('dev'))
 
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+})
 
+io.on('connection', (socket) => {
+    console.log('User Connected', socket.id)
+
+    socket.on('message', (message) => {
+        console.log('Message', message)
+        
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected', socket.id)
+    })
+})
 
 // Routes
 app.use('/auth',authRoutes)
 app.use('/workspaces', workspaceRoutes)
 
-
-
-module.exports = app
+httpServer.listen(3000, () => {
+    console.log('App is running')
+})
