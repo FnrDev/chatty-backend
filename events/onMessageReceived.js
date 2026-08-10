45e3746644a752a12f879a14054381da54a60981
Ahmed <fnr5541@gmail.com>
2026-08-10 17:48:03 +0300

    handle on message received websocket

--- events/onMessageReceived.js
+++ events/onMessageReceived.js
@@ -1,5 +1,33 @@
+const { Message } = require("../models/workspace")
+const { getIO } = require('../socket')
+
 async function onMessageReceived(message) {
     console.log(message)
+    // BIG SECURITY RISK, I KNOW!!!
+    const {
+        textContent,
+        mediaURL,
+        mediaMimeType,
+        mentionEveryone,
+        replyTo,
+        channelId,
+        workspaceId,
+        author
+    } = message
+
+    const createdMessage = await Message.create({
+        textContent,
+        mediaMimeType,
+        mediaURL,
+        mentionEveryone,
+        author,
+        channel: channelId,
+        workspace: workspaceId,
+    })
+
+    const popluateAuthor = await Message.findById(createdMessage._id).populate('author')
+
+    getIO().emit('message_received', popluateAuthor)
 }
 
 module.exports = onMessageReceived
--- routes/channels.routes.js
+++ routes/channels.routes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router({ mergeParams: true })
 const verifyToken = require("../middleware/verifyToken")
 const channelsController = require("../controllers/channels.controller")
+const messageController = require('../controllers/messages.controller')
 
 router.get('/', verifyToken, channelsController.getAllChannels)
 router.get('/:id', verifyToken, channelsController.getChannelByID)
 router.post('/', verifyToken, channelsController.createChannel)
+
+router.get('/:channelId/messages', verifyToken, messageController.getChannelMessages)
+router.post('/:channelId/messages', verifyToken, messageController.createChannelMessage)
 
 module.exports = router
